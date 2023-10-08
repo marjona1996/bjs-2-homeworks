2dd30b094cefad6a6680af11b2d294adf8de7060
Marjonka <marjona.abdullaeva@inbox.ru>
2023-10-08 10:01:46 +0500

    Второе задание готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,20 +1,20 @@
 function getArrayParams(...arr) {
-  function getArrayParams(...arr) {
-    let min = Infinity;
-    let max = -Infinity;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > max) max = arr[i];
-      if (arr[i] < min) min = arr[i];
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
     }
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-      sum += arr[i];
+    if (arr[i] < min) {
+      min = arr[i];
     }
-    let avg = Number((sum / arr.length).toFixed(2));
-    return { min: min, max: max, avg: avg };
+    sum += arr[i];
   }
-  console.log(getArrayParams(-99, 99, 10));
+  let avg = Number((sum / arr.length).toFixed(2));
+  return { min: min, max: max, avg: avg };
 }
+console.log(getArrayParams(-99, 99, 10));
 
 function summElementsWorker(...arr) {
 
