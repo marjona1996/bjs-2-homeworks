33e5d7d52ac02154f12226670073afb97b46ba2f
Marjonka <marjona.abdullaeva@inbox.ru>
2023-10-19 19:08:53 +0500

    Четвёртое задание почти готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+	this.name = name,
+		this.gender = gender,
+		this.age = age,
+		marks = [],
 }
+let user = new Student("Anna", "famel", 36);
+
 
 Student.prototype.setSubject = function (subjectName) {
-  
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function (...marksToAdd) {
+	if (Student.marks) {
+		console.log(Student.marks);
+	} if (subject === this.excluded) {
+		delete this.marks;
+		delete this.subject;
+	}
+	this.subject = marksToAdd;
 
+}
 Student.prototype.getAverage = function () {
-  
+
 }
 
 Student.prototype.exclude = function (reason) {
-  
+
 }
